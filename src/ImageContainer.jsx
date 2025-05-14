80e6e51ce3751780e2d5bae8cbbb0a6dd6d1af4e
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-05-14 23:53:38 +0530

    vercel correction

--- src/ImageContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useGLTF } from "@react-three/drei";
-
-const ImageContainer = () => {
-  const { scene } = useGLTF("/mac.glb"); 
-  return <primitive object={scene} />;
-};
-
-export default ImageContainer;
